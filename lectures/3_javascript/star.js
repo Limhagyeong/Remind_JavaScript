0857c9db8b431932a509efa00a2f9ced8b1e3fad
Limhagyeong <aamumu88@gmail.com>
2024-10-25 15:05:15 +0900

    star

---
+++ lectures/3_javascript/star.js
@@ -0,0 +1,68 @@
+'use strict';
+
+/*
+1) 정사각형
+*****
+*****
+*****
+*****
+*****
+
+*/
+
+// for (let i = 0; i < 5; i++) {
+//   for (let j = 0; j < 5; j++) {
+//     document.write('*');
+//   }
+//   document.write('<br>');
+// }
+
+/*
+2) 삼각형
+*
+**
+***
+****
+*****
+
+*/
+
+// for (let i = 0; i < 5; i++) {
+//   for (let j = 0; j < i + 1; j++) {
+//     document.write('*');
+//   }
+//   document.write('<br>');
+// }
+
+/*
+
+3) 삼각형 - reverse
+
+*****
+****
+***
+**
+*
+
+*/
+
+/*
+  삼각형 2
+
+    *
+   **
+  ***
+ ****
+*****
+
+*/
+
+// for (let i = 0; i < 5; i++) {
+//   for (let j = 4; j > i; j--) {
+//     document.write('&nbsp;');
+//   }
+//   for (let k = 0; k <= i; k++) {
+//     document.write('*');
+//   }
+//   document.write('<br>');
+// }
